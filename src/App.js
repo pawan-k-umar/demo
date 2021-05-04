10c92d4766a9cf6f101177b80659858a613e60ca
pawan-kr <itsmekr.info@gmail.com>
2021-05-04 11:12:03 +0530

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Nav from './component/Nav';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+    <Nav/>
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
